aab0e995a7c7c74250f435de53743e0c11d31dd3
PetyaKatsarova <pskpetya@gmail.com>
2024-09-13 10:13:40 +0200

    added chapter 12: abt classes and interfaces

---
+++ snippets/src/main/java/org/example/snippets/Enums3.java
@@ -0,0 +1,23 @@
+package org.example.snippets;
+
+public class Enums3 {
+    enum Flavors {
+        CHOCOLATE, STRAWBERRY, VANILLA
+    }
+
+//    public static void main(String[] args) {
+//        Flavors STRAWBERRY = null;
+//        switch (STRAWBERRY) {
+//            case Flavors.VANILLA:
+//                System.out.print("v");
+//            case Flavors.CHOCOLATE:
+//                System.out.print("c");
+//            case Flavors.STRAWBERRY:
+//                System.out.print("s");
+//                break;
+//            default:
+//                System.out.println("missing flavor");
+//        }
+//    }
+
+}
---
+++ snippets/src/main/java/org/example/snippets/Lamba1.java
@@ -0,0 +1,16 @@
+package org.example.snippets;
+
+public class Lamba1 {
+    @FunctionalInterface
+    interface MyFunction {
+        int apply(int a, int b);
+    }
+
+    static void execute(MyFunction f) {
+        System.out.println(f.apply(5, 3)); // Executes lambda
+    }
+    public static void main(String[] args) {
+        execute((a, b) -> a * b);
+
+    }
+}
